3ba9e54f4ed096a949b7e6df14060386f63f3669
AznRaven <versatilias@gmail.com>
2023-03-04 22:34:39 -0500

    test

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,39 +1,43 @@
 export default function About(test) {
-  return (
-    <>
-      {/* Sidebar */}
-      <div
-        class="offcanvas offcanvas-end show"
-        tabindex="-1"
-        id="offcanvas"
-        aria-labelledby="offcanvasLabel"
-      >
-        <div class="offcanvas-header">
-          <h5 class="offcanvas-title" id="offcanvasLabel">
-            Settings
-          </h5>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="offcanvas"
-            aria-label="Close"
-          ></button>
+    function toggleOffcanvas() {
+      document.querySelector("#offcanvas").classList.toggle("show");
+    }
+  
+    return (
+      <>
+        {/* Sidebar */}
+        <div
+          class="offcanvas offcanvas-end"
+          tabindex="-1"
+          id="offcanvas"
+          aria-labelledby="offcanvasLabel"
+        >
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasLabel">
+              Settings
+            </h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="offcanvas"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="offcanvas-body">
+            Content for the offcanvas goes here. You can place just about any
+            Bootstrap component or custom elements here.
+          </div>
         </div>
-        <div class="offcanvas-body">
-          Content for the offcanvas goes here. You can place just about any
-          Bootstrap component or custom elements here.
-        </div>
-      </div>
-      {/* Sidebar */}
-      <button
-        class="btn btn-primary"
-        type="button"
-        data-bs-toggle="offcanvas"
-        data-bs-target="#offcanvas"
-        aria-controls="offcanvasExample"
-      >
-        Sidebar
-      </button>
-    </>
-  );
-}
+        {/* Sidebar */}
+        <button
+          class="btn btn-primary"
+          type="button"
+          onClick={toggleOffcanvas}
+          aria-controls="offcanvasExample"
+        >
+          Sidebar
+        </button>
+      </>
+    );
+  }
+  
