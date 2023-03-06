da46da7b93c0c7ded223b8ea89b9c238355ca24a
AznRaven <versatilias@gmail.com>
2023-03-06 10:51:25 -0500

    footer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
+import Footer from "./components/Footer";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -11,6 +12,7 @@ root.render(
   <StrictMode>
     <Router>
       <App />
+      <Footer/>
     </Router>
   </StrictMode>
 );
