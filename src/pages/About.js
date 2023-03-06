7f29e2612451e44c2093c1953f1171740e2ff1e4
AznRaven <versatilias@gmail.com>
2023-03-06 11:52:27 -0500

    about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,48 @@
 export default function About(test) {
-    
-  
-    return (
-      <>
-        <h1>Giphy</h1>
-        <p>Giphy, styled as GIPHY, is an American online database and search engine that allows users to search for and share short looping videos with no sound that resemble animated GIF files.</p>
-        <br/>
-        <h1>Giphy</h1>
-        <p>Giphy, styled as GIPHY, is an American online database and search engine that allows users to search for and share short looping videos with no sound that resemble animated GIF files.</p>
-      </>
-    );
-  }
-  
+  return (
+    <>
+      <h1>Giphy</h1>
+      <p>
+        Giphy, styled as GIPHY, is an American online database and search engine
+        that allows users to search for and share short looping videos with no
+        sound that resemble animated GIF files.
+      </p>
+      <br />
+      <h1>Marvel</h1>
+      <p>
+        The Marvel Comics API allows developers everywhere to access information
+        about Marvel's vast library of comics—from what's coming up, to 70 years
+        ago.
+      </p>
+      <br />
+      <h1>News</h1>
+      <p>
+        Locate articles and breaking news headlines from news sources and blogs
+        across the web.
+      </p>
+      <br />
+      <h1>Pokemon</h1>
+      <p>
+        Locate articles and breaking news headlines from news sources and blogs
+        across the web.
+      </p>
+      <br />
+      <h1>Rick And Morty</h1>
+      <p>
+        The Rick and Morty API is a REST(ish) and GraphQL API based on the
+        television show Rick and Morty. You will have access to about hundreds
+        of characters, images, locations and episodes. The Rick and Morty API is
+        filled with canonical information as seen on the TV show.
+      </p>
+      <br />
+      <h1>Weather</h1>
+      <p>
+        Access current weather data for any location on Earth including over
+        200,000 cities! We collect and process weather data from different
+        sources such as global and local weather models, satellites, radars and
+        a vast network of weather stations. Data is available in JSON, XML, or
+        HTML format.
+      </p>
+    </>
+  );
+}
