29cef67f5504604dc7c6de39cffdd1c8e2e4dafa
AznRaven <versatilias@gmail.com>
2023-03-06 11:44:56 -0500

    about

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,11 @@ export default function About(test) {
   
     return (
       <>
-        
+        <h1>Giphy</h1>
+        <p>Giphy, styled as GIPHY, is an American online database and search engine that allows users to search for and share short looping videos with no sound that resemble animated GIF files.</p>
+        <br/>
+        <h1>Giphy</h1>
+        <p>Giphy, styled as GIPHY, is an American online database and search engine that allows users to search for and share short looping videos with no sound that resemble animated GIF files.</p>
       </>
     );
   }
