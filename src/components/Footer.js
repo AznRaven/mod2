3d370d1b16c3b648ac0d84b415aec68ad368acf8
AznRaven <versatilias@gmail.com>
2023-03-06 10:56:24 -0500

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
 export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
-    <>
+    <div className="footer">
       <p>Author: Hai Hoang</p>
       <p>© {currentYear} AznRaven's Api</p>
-    </>
+    </div>
   );
 }
